70ae03149896333267600dffeb00f66e1b217f00
ndc-webapps <nadelacruz01@gmail.com>
2026-04-05 16:12:53 +0800

    Add files via upload

---
+++ email.min.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>CDN | EmailJS</title>
+    </head>
+    <body>
+        <p>
+            Support for <b>cdn.emailjs.com</b> will be phased out in the near future.
+            <br>
+            <b>Switch to the new SDK to avoid service disruption.</b>
+        </p>
+        <p>
+            For more information, visit
+            <a href="https://www.emailjs.com/docs/sdk/installation/">
+                https://www.emailjs.com/docs/sdk/installation/
+            </a>
+        </p>
+    </body>
+</html>
